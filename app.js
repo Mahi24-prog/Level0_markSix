4a76d1637afb182a3085cf2bd15ce2400f68e93f
Mahendra Balu Pawar <mahendrapawar444666@gmail.com>
2021-09-08 20:12:38 +0530

    some logic changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var inputText = document.querySelector("#input-txt");
 var transBtn = document.querySelector(".trans-btn");
-var outputText = document.querySelector("#outputText");
+var outputText = document.querySelector("#output-txt");
 
 var url = "https://api.funtranslations.com/translate/minion.json";
 
@@ -14,5 +14,5 @@ function convertUrl(txt){
 function translate(){
     var finalUrl = convertUrl(inputText.value)
 
-    fetch(finalUrl).then(Response => Response.json()).then(json => console.log(json.contents.translated)).catch((err)=>{console.log(err)})
+    fetch(finalUrl).then(Response => Response.json()).then(json => {outputText.innerText = json.contents.translated}).catch((err)=>{console.log(err)})
 }
